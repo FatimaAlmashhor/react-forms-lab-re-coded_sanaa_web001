bcf5f4735e24c4a049e0f6b60c35a87b9757f979
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-20 04:50:03 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message:"",
+      remainingChars:0
+    };
   }
-
+  handleChange=(e)=>{
+    if(e.target.value.length<=this.props.maxChars){
+      this.setState(({message:e.target.value,
+      remainingChars:e.target.value.length}))
+      
+    }
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        <span>Remaining chars: {this.props.maxChars-this.state.remainingChars}</span>
+        </div>
     );
   }
 }
-
-export default TwitterMessage;
